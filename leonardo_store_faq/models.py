68a6ec89a1b361db67aba2816ea001a4c4f645dc
Michael Kutý <6du1ro.n@gmail.com>
2016-10-13 17:49:31 +0200

    Fix question represenation.

--- leonardo_store_faq/models.py
+++ leonardo_store_faq/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from django.utils.encoding import python_2_unicode_compatible, force_text
 from django.utils.translation import pgettext_lazy, ugettext_lazy as _
 
 
 @python_2_unicode_compatible
 class Question(models.Model):
-    name = models.CharField(max_length=255, verbose_name=pgettext_lazy("Inanimate object", "Name"))
+    name = models.CharField(
+        max_length=255, verbose_name=pgettext_lazy("Inanimate object", "Name"))
     question_text = models.TextField(verbose_name=_("Question text"))
     answer = models.TextField(verbose_name=_("Answer"))
 
@@ -23,10 +24,13 @@ class ProductQuestion(models.Model):
         Question, related_name="product_questions", verbose_name=_("Question"))
     product = models.ForeignKey("catalogue.Product", related_name="questions",
                                 verbose_name=_("Product"))
-    order = models.IntegerField(null=True, blank=True, verbose_name=pgettext_lazy("As in ordering", "Order"))
+    order = models.IntegerField(
+        null=True, blank=True, verbose_name=pgettext_lazy("As in ordering", "Order"))
 
     def __str__(self):
-        return " - ".join([self.question, self.product, self.order])
+        return " - ".join([force_text(self.question),
+                           self.product,
+                           self.order])
 
     class Meta:
         verbose_name = _("Product question")
